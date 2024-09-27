5f82f1e23306bc009a6655a144731b163078e475
GiorgosMarga <giorgosmarga509@gmail.com>
2024-09-27 14:16:42 +0300

    fixed bugs for deployment

--- app/activate/page.tsx
+++ app/activate/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 import { Spinner } from '@nextui-org/react'
 import { useSearchParams } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import { makeRequest } from '../request'
 import { StatusCodes } from 'http-status-codes'
 
-export default function ActivatePage() {
+function Activate() {
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState("")
     const searchParams = useSearchParams()
@@ -18,7 +18,7 @@ export default function ActivatePage() {
             if (res.statusCode === StatusCodes.OK) {
                 setError("Your account has been activated. Soon you will be redirected....")
             } else if (res.statusCode === StatusCodes.UNPROCESSABLE_ENTITY) {
-                setError(res.body?.token ?? "There was an error. Please try agin later.")
+                setError(res.body?.token ?? "There was an error. Please try again later.")
             }
             setIsLoading(false)
         }
@@ -32,5 +32,13 @@ export default function ActivatePage() {
             {isLoading ? <Spinner color='danger' /> : null}
             {error.length === 0 ? <p className='text-lg mt-5 text-danger'>Activating account...</p> : <p className='text-sm text-danger font-semibold'>{error}</p>}
         </div>
+    )
+}
+
+export default function ActivatePage() {
+    return (
+        <Suspense>
+            <Activate />
+        </Suspense>
     )
 }
